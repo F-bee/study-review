01e481530d0d3a4b8d80fc820706934b57c617a1
kk <71758825+F-bee@users.noreply.github.com>
2023-01-08 21:18:45 +0800

    Delete src/com/kk directory

--- src/com/kk/constant/SeqListConstant.java
+++
@@ -1,7 +0,0 @@
-package com.kk.constant;
-
-public class SeqListConstant {
-
-    public static final Integer DEFAULT_SIZE = 16;
-
-}
--- src/com/kk/exception/OperationException.java
+++
@@ -1,7 +0,0 @@
-package com.kk.exception;
-
-public class OperationException extends RuntimeException{
-    public OperationException(String message) {
-        super(message);
-    }
-}
